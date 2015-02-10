f1e45240cd807a19248c640baa5ca8525830b6da
Joshua <jbkunst@gmail.com>
2015-02-10 18:03:13 -0300

    idk! :sad:

--- 009-Simple-Force-Layout-Algorithm/script.R
+++ 009-Simple-Force-Layout-Algorithm/script.R
@@ -16,15 +16,13 @@ library("grid")
 #                 0,0,0,0,1,1,0),
 #               byrow = TRUE, nrow = 7)
 
-net <- matrix(c(0,1,0,0,
-                1,0,1,1,
-                0,1,0,1,
-                0,1,1,0),
+net <- matrix(c(0,1,1,0,
+                1,0,1,0,
+                1,1,0,1,
+                0,0,1,0),
               byrow = TRUE, nrow = 4)
 
-c0 <- 0.5
-c1 <- 0.9
-l <- 0.5
+K <- C <- 1
 
 net <- as.matrix.network(network(net), matrix.type="edgelist")
 
@@ -58,12 +56,12 @@ net.calculate.repulsive <- function(net){
              node_x = node_x,
              node_y = node_y,
              node_force = setdiff(nodes, node),
-             dist = (node_x - V1)^2 + (node_y - V2)^2,
+             dist = (V1 - node_x)^2 + (V2 - node_y)^2,
              dist = sqrt(dist),
-             ex = (node_x - V1)/dist,
-             ey = (node_y - V2)/dist,
-             fx = c0*ex/dist^2,
-             fy = c0*ey/dist^2)
+             ex = (V1 - node_x)/dist,
+             ey = (V2 - node_y )/dist,
+             fx = -C*K^2*ex/dist,
+             fy = -C*K^2*ey/dist)
   })
   
   drep
@@ -93,12 +91,12 @@ net.calculate.attractive <- function(net){
              node_x = node_x,
              node_y = node_y,
              node_force = relations_edges,
-             dist = (node_x - V1)^2 + (node_y - V2)^2,
+             dist = (V1 - node_x)^2 + (V2 - node_y)^2,
              dist = sqrt(dist),
-             ex = (node_x - V1)/dist,
-             ey = (node_y - V2)/dist,
-             fx = -c1*(dist - l)*ex,
-             fy = -c1*(dist - l)*ey)
+             ex = (V1 - node_x)/dist,
+             ey = (V2 - node_y )/dist,
+             fx = ex*dist^2/K,
+             fy = ey*dist^2/K)
   })
 }
 
@@ -121,22 +119,17 @@ net.plot <- function(net){
     cbind( dpositions[dedges$V1,1:2] %>% setNames(c("xs", "ys"))) %>%
     cbind( dpositions[dedges$V2,1:2] %>% setNames(c("xe", "ye")))
   
-  ggplot() +
-    geom_point(data=dpositions, aes(V1, V2), size = 10, alpha = 0.7, color="black") + # nodes
-    geom_segment(data=dedges, aes(x=xs, y=ys, xend=xe, yend=ye), alpha = 0.7, size = 2, color = "black") + # edges
-    
-#     geom_segment(data=drep ,aes(x=node_x, y=node_y, xend=node_x+fx, yend=node_y+fy), alpha= 0.1, arrow = arrow(), color ="darkred", size=0.5) +
-#     geom_segment(data=drep2,aes(x=node_x, y=node_y, xend=node_x+fx, yend=node_y+fy), alpha= 0.3, arrow = arrow(), color ="darkred", size=1.2) +
-#     
-#     geom_segment(data=datt ,aes(x=node_x, y=node_y, xend=node_x+fx, yend=node_y+fy), alpha= 0.1, arrow = arrow(), color="darkblue", size=0.5) +
-#     geom_segment(data=datt2,aes(x=node_x, y=node_y, xend=node_x+fx, yend=node_y+fy), alpha= 0.3, arrow = arrow(), color="darkblue", size=1.2) +
-    
-    geom_segment(data=dforce,aes(x=node_x, y=node_y, xend=node_x+fx, yend=node_y+fy), alpha= 0.5, arrow = arrow(), color="green", size=1.2) +
-    
-    # theme
-    geom_text(data=dpositions, aes(V1, V2, label=names), color = "white") +
-    theme_gray()
-   
+  p <- ggplot()
+  p <- p + geom_point(data=dpositions, aes(V1, V2), size = 10, alpha = 0.7, color="black")
+  p <- p + geom_segment(data=dedges, aes(x=xs, y=ys, xend=xe, yend=ye), alpha = 0.7, size = 2, color = "black")
+#   p <- p + geom_segment(data=drep ,aes(x=node_x, y=node_y, xend=node_x+fx, yend=node_y+fy), alpha= 0.1, arrow = arrow(), color ="darkred", size=0.5)
+#   p <- p + geom_segment(data=drep2,aes(x=node_x, y=node_y, xend=node_x+fx, yend=node_y+fy), alpha= 0.3, arrow = arrow(), color ="darkred", size=1.2)
+#   p <- p + geom_segment(data=datt ,aes(x=node_x, y=node_y, xend=node_x+fx, yend=node_y+fy), alpha= 0.1, arrow = arrow(), color="darkblue", size=0.5)
+#   p <- p + geom_segment(data=datt2,aes(x=node_x, y=node_y, xend=node_x+fx, yend=node_y+fy), alpha= 0.3, arrow = arrow(), color="darkblue", size=1.2)
+#   p <- p + geom_segment(data=dforce,aes(x=node_x, y=node_y, xend=node_x+fx, yend=node_y+fy), alpha= 0.5, arrow = arrow(), color="green", size=1.2)
+  p <- p + geom_text(data=dpositions, aes(V1, V2, label=names), color = "white")
+  p <- p + theme_gray()
+  p
 }
 
 net.plot(net)
@@ -162,7 +155,6 @@ net.update.positions <- function(net){
   net   
   
 }
-
 
 
 net.plot(net)
