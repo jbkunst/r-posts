d8472af87bbd4d7ba5f9b196ac0dad602d09595a
Joshua <jbkunst@gmail.com>
2015-08-10 23:45:06 -0300

    rhm... ok

--- 22_gg_cartesian/readme.R
+++ 22_gg_cartesian/readme.R
@@ -1,6 +1,7 @@
 #' ---
 #' output:
-#'   md_document
+#'   html_document:
+#'     keep_md: yes
 #' ---
 
 library("ggplot2")
@@ -10,8 +11,9 @@ library("dplyr")
 library("grid")
 
 rm(list = ls())
-size = 10; by = 2
-
+size <- 5
+by <- 1
+tick.size <- 0.1
 
 gg_null <- function(){
  
@@ -21,7 +23,11 @@ gg_null <- function(){
   
 }
 
-df_cart_axis <- function(size = 10){
+df_cart_axis <- function(size = 5){
+  
+  offset <- 0.5
+  
+  size <- size + offset
   
   data_frame(x = c(-size, 0),
              y = c(0, -size),
@@ -30,7 +36,7 @@ df_cart_axis <- function(size = 10){
   
 }
 
-df_cart_labels <- function(size = 10, by = 2, size.ticks = 0.1){
+df_cart_labels <- function(size = 5, by = 1){
   
   axis <- seq(0, size, by = by)
   axis <- setdiff(axis, 0)
@@ -42,31 +48,86 @@ df_cart_labels <- function(size = 10, by = 2, size.ticks = 0.1){
                           y = c(axis0, axis),
                           label = ifelse(x != 0, x, y))
   
-#   df_labels <- df_labels %>%
-#     mutate(label = str_pad(label, side = "left", width = max(nchar(label))))
-#   
   df_labels 
   
 }
 
-gg_cartesian <- function(size = 10, by = 2){
+df_cart_ticks <- function(size = 5, by = 1, tick.size = 0.1){
+ 
+  df_labels <- df_cart_labels(size, by)
   
-  df_cart <- df_cart_axis(size)
-  df_lbls <- df_cart_labels(size, by)
+  df_tcks_x <- df_labels %>% 
+    filter(y == 0) %>% 
+    select(x, y) %>% 
+    mutate(xend = x, yend = y - tick.size, y = y + tick.size)
+    
+  df_tcks_y <- df_labels %>% 
+    filter(x == 0) %>% 
+    select(x, y) %>% 
+    mutate(yend = y, xend = x - tick.size, x = x + tick.size)
   
-  graphics.off()
-
-  gg_null() +
-    geom_segment(data = df_cart,
-                 aes(x, y, xend = xend, yend = yend),
-                 arrow = arrow(length = unit(0.25, "cm"), ends = "both"),
-                 color = "gray10") + 
-    geom_text(data = df_lbls %>% filter(x == 0), aes(x + 1, y, label = label)) + 
-    geom_text(data = df_lbls %>% filter(y == 0), aes(x, y - 1, label = label))  
-  
+  rbind(df_tcks_x, df_tcks_y)
   
 }
 
-gg_cartesian()
+df_cart_grid <- function(size = 5, by = 1){
+  
+  df_lines <- data_frame(x = seq(-size, size, by),
+                         y = x) %>% 
+    filter(x != 0)
+  
+}
 
+gg_cartesian <- function(size = 5, by = 1, tick.size = 0.1,
+                         axis = TRUE, labels = TRUE, ticks = TRUE, grid = TRUE){
+  
+  p <- gg_null()
+  
+  if(grid){
+    df_grid <- df_cart_grid(size, by)
+    p <- p + 
+      geom_hline(data = df_grid, aes(yintercept = x), color = "gray70", size = 0.3) +
+      geom_vline(data = df_grid, aes(xintercept = y), color = "gray70", size = 0.3)
+    
+  }
+  
+  if(ticks) {
+    
+    df_tcks <- df_cart_ticks(size, by, tick.size)
+    
+    p <- p + geom_segment(data = df_tcks, aes(x, y, xend = xend, yend = yend), size = 1.01) 
+      
+  }
+  
+  if(axis){
+    
+    df_cart <- df_cart_axis(size)
+    
+    p <- p +
+      geom_segment(data = df_cart,
+                   aes(x, y, xend = xend, yend = yend),
+                   arrow = arrow(length = unit(0.30, "cm"), ends = "both"),
+                   color = "gray10", size = 1.01) 
+    
+  }
+  
+  if(labels){
+    
+    df_lbls <- df_cart_labels(size, by)
+    
+    p <- p + 
+      geom_text(data = df_lbls %>% filter(x == 0), aes(x + 0.5, y, label = label)) + 
+      geom_text(data = df_lbls %>% filter(y == 0), aes(x, y - 0.5, label = label))
+  
+  }
+ 
+  p
+  
+}
+
+
+gg_cartesian()
+gg_cartesian(size = 10, by = 2)
+
+gg_cartesian(labels = FALSE)
 
