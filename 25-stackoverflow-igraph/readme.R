70c8bc9afd8b615a1664c43f9778992db8b1ff37
Joshua <jbkunst@gmail.com>
2015-10-01 02:23:11 -0300

    6000 questions?

--- 25-stackoverflow-igraph/readme.R
+++ 25-stackoverflow-igraph/readme.R
@@ -1,57 +1,100 @@
 rm(list = ls())
 library("httr")
+library("igraph")
 library("plyr")
 library("dplyr")
 library("purrr")
-library("igraph")
 library("ggplot2")
+library("tidyr")
 
 
+# From https://api.stackexchange.com/docs/questions
 
 # Global Parameters
 SITE <- "stackoverflow"
 TAG <- "r"
-NPAGES <- 10
+NPAGES <- 500 # 500 # 3600
 API_URL <- "https://api.stackexchange.com/2.2/questions"
 NTAGS <- 50
 
 # Question List
-qlist <- seq(PAGES) %>% 
+qlist <- seq(NPAGES) %>% 
   map(function(p){
     # p <- 2
     data <- API_URL %>% 
-      GET(query = list(site = SITE, tagged = TAG, page = p)) %>% 
+      GET(query = list(site = SITE, tagged = TAG, page = p, sort = "creation")) %>% 
       content()
     data$items
   }) %>% 
   unlist(recursive = FALSE)
 
+# saveRDS(qlist, "qlist.Rds")
+qlist <- readRDS("qlist.Rds")
 
-# Question Tags Dataframe
-qst_tgs <- ldply(question_list, function(x){
-  # x <- sample(question_list, size = 1)[[1]]
+
+# Dataframe with question_id, question_tag
+df_qtag <- ldply(qlist, function(x){
+  # x <- sample(qlist, size = 1)[[1]]
   tags <- x$tags %>% unlist()
   if (length(tags) > 1) {
     data_frame(question_tag = setdiff(tags, TAG),
                question_id = x$question_id)  
   }
-}, .progress = "win")
+}, .progress = "tk")
 
-qst_tgs <- tbl_df(qst_tgs)
+df_qtag <- tbl_df(df_qtag)
 
-dftags <- qst_tgs %>%
-  count(question_tag) %>%
+toptags <- df_qtag %>%
+  count(question_tag) %>% 
   arrange(desc(n)) %>% 
-  mutate(question_tag = factor(question_tag, levels = rev(question_tag)))
+  head(NTAGS) %>%
+  mutate(question_tag = factor(question_tag, rev(question_tag)))
 
-head(dftags)
+toptags
 
-ggplot(dftags %>% head(50)) +
+ggplot(toptags) + 
   geom_bar(aes(question_tag, n), stat = "identity", width = 0.5) + 
   coord_flip()
 
 
+# Examine a element in the list:
+x <- sample(qlist, size = )[[1]]
+str(x)
+
+classes <- map(x, class) %>%
+  dplyr::as_data_frame() %>% 
+  gather(name, class)
+
+classes
+
+namestoselc <-  classes %>%
+  filter(class != "list" & name != "link") %>%
+  .$name
+
+namestoselc
 
 
+# Dataframe Questions
+df_qst <- ldply(qlist, function(x){
+  # x <- sample(qlist, size = 1)[[1]]
+  x[ which(names(x) %in% namestoselc)] %>% 
+    as_data_frame()
+}, .progress = "win")
+
+df_qst <- tbl_df(df_qst)
+
+df_qst <- df_qst %>% 
+  mutate(creation_date_2 = as.POSIXct(creation_date, origin="1970-01-01"))
+
+df_qst$creation_date_2 %>% summary()
+
+
+df_date_tags <- df_qst %>%
+  select(question_id, creation_date_2) %>% 
+  inner_join(df_qtag) %>% 
+  filter(as.Date(creation_date_2) > as.Date("2015-01-01"))
+
+ggplot(df_date_tags) + 
+  geom_density(aes(creation_date_2, fill = question_tag))
 
 
