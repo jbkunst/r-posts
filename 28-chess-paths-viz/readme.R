815dbcb569d632b77c7e3c7135ed31fc36e18c7a
Joshua <jbkunst@gmail.com>
2015-10-16 10:34:17 -0300

    testing angle parameter in ggplot::geom_curve

--- 28-chess-paths-viz/readme.R
+++ 28-chess-paths-viz/readme.R
@@ -15,44 +15,41 @@ library("dplyr")
 library("readr")
 library("ggplot2")
 
-dfgames <- read_csv("data/chess_magnus.csv.gz")
-
-dfgames <- dfgames %>% head(125)
-
-problems(dfgames)
-
-dfgames  <- dfgames %>% mutate(game_id = seq(nrow(.)))
-
-
-t0 <- Sys.time()
-
-dfmoves <- dfgames %>%
-  group_by(game_id) %>% 
-  do({
-    mvs <- .$moves %>% strsplit(" ") %>% unlist()
-    chss <- Chess$new()
-    
-    for (mv in mvs) chss$move(mv)
-    
-    dfhist <- chss$history(verbose = TRUE) %>% 
-      mutate(number_move = seq(nrow(.)))
-    
-    dfhist
-    
-  }) %>%
-  ungroup()
-
-t1 <- Sys.time() - t0
+# dfgames <- read_csv("data/chess_magnus.csv.gz")
+# 
+# dfgames <- dfgames %>% head(125)
+# 
+# problems(dfgames)
+# 
+# dfgames  <- dfgames %>% mutate(game_id = seq(nrow(.)))
+# 
+# 
+# t0 <- Sys.time()
+# 
+# dfmoves <- dfgames %>%
+#   group_by(game_id) %>% 
+#   do({
+#     mvs <- .$moves %>% strsplit(" ") %>% unlist()
+#     chss <- Chess$new()
+#     
+#     for (mv in mvs) chss$move(mv)
+#     
+#     dfhist <- chss$history(verbose = TRUE) %>% 
+#       mutate(number_move = seq(nrow(.)))
+#     
+#     dfhist
+#     
+#   }) %>%
+#   ungroup()
+# 
+# t1 <- Sys.time() - t0
 # 
 # saveRDS(dfgames, file = "data/dfgames.rds")
 # saveRDS(dfmoves, file = "data/dfmoves.rds")
 # 
 # #' Load data
-# dfgames <- readRDS("data/dfgames.rds")
-# dfmoves <- readRDS("data/dfmoves.rds")
-
-#' Plots a chess data
-chessboardjs()
+dfgames <- readRDS("data/dfgames.rds")
+dfmoves <- readRDS("data/dfmoves.rds")
 
 #' Example Game
 dfmvex <- dfmoves %>% filter(game_id == 1)
@@ -62,7 +59,10 @@ dfmvex
 chss <- Chess$new()
 chss$clear()
 chss$put("n", "w", "g1")
-plot(chss)
+
+plot(chss, type = "ggplot")
+
+chss$move("Nf3")$plot(type = "ggplot")
 
 
 #' Some details of parameters
@@ -74,7 +74,7 @@ path <- NULL
 path <- c(path, pos_current)
 
 while (!piece_was_captured) {
-  message(pos_current, " ", pos_nummove)
+  # message(pos_current, " ", pos_nummove)
   # Maybe the piece was capture and have no movements :(,
   # thats why check from and to variable.
   dfmvex_aux <- dfmvex %>%
@@ -95,26 +95,38 @@ while (!piece_was_captured) {
   
 }
 
-#' Creating data to plot
-dfboard <- rchess:::.chessboarddata() %>% select(cell, col, row, x, y, cc)
+
+path 
 
 dfpaths <- data_frame(from = head(path, -1),
                       to = tail(path, -1))
 
+dfpaths
+
+#' Creating data to plot
+dfboard <- rchess:::.chessboarddata() %>% select(cell, col, row, x, y, cc)
+
+dfboard
+
+
 dfpaths <- dfpaths %>% 
-  left_join(dfboard %>% rename(from = cell), by = "from") %>% 
-  left_join(dfboard %>% rename(to = cell) %>% select(-cc), by = "to") %>% 
-  mutate(curve = ifelse((x.x - x.y) < 0, TRUE, FALSE))
+  left_join(dfboard %>% rename(from = cell, x.from = x, y.from = y), by = "from") %>% 
+  left_join(dfboard %>% rename(to = cell, x.to = x, y.to = y) %>% select(-cc, -col, -row), by = "to") %>% 
+  mutate(curve = ifelse((x.from - x.to) < 0, TRUE, FALSE))
 
 
 ggplot() +
   geom_tile(data = dfboard, aes(x, y, fill = cc)) +
   geom_curve(data = dfpaths %>% filter(curve),
-             aes(x = x.x, y = y.x, xend = x.y, yend = y.y), color = "white", alpha = 0.5, curvature = -.3,
-             arrow = arrow(length = unit(0.25,"cm")), position =  position_jitter(w = 0.25, h = 0.25)) +
+             aes(x = x.from, y = y.from, xend = x.to, yend = y.to),
+             color = "white", alpha = 0.5, curvature = 1, angle = -55,
+             arrow = arrow(length = unit(0.25,"cm")),
+             position =  position_jitter(width = 0.25, height = 0.25)) +
   geom_curve(data = dfpaths %>% filter(!curve),
-             aes(x = x.x, y = y.x, xend = x.y, yend = y.y), color = "white", alpha = 0.25, curvature = .3,
-             arrow = arrow(length = unit(0.25,"cm")), position =  position_jitter(w = 0.25, h = 0.25)) +
+             aes(x = x.from, y = y.from, xend = x.to, yend = y.to),
+             color = "white", alpha = 0.5, curvature = .3,
+             arrow = arrow(length = unit(0.25,"cm")),
+             position =  position_jitter(width = 0.25, height = 0.25)) +
   scale_fill_manual(values =  c("gray10", "gray20")) +
   coord_equal() + 
   ggthemes::theme_map() +
@@ -122,76 +134,84 @@ ggplot() +
 
 
 
-dfpaths <- dfmoves %>% 
-  group_by(game_id) %>% 
-  do({
-    dfmvex <- .
-    #' Some details of parameters
-    pos_start <- "g1"
-    pos_current <- pos_start
-    pos_nummove <- 0
-    piece_was_captured <- FALSE
-    path <- NULL
-    path <- c(path, pos_current)
-    
-    while (!piece_was_captured) {
-      dfmvex_aux <- dfmvex %>%
-        filter(from == pos_current | to == pos_current,
-               number_move > pos_nummove) %>% 
-        head(1)
-      
-      if (nrow(dfmvex_aux) == 0) break
-      if (dfmvex_aux$to == pos_current) break
-      
-      pos_current <- dfmvex_aux$to
-      pos_nummove <- dfmvex_aux$number_move
-      
+get_paths_from_position <- function(position) {
+  
+  dfresponse <- dfmoves %>% 
+    group_by(game_id) %>% 
+    do({
+      dfmvex <- .
+      #' Some details of parameters
+      pos_start <- position
+      pos_current <- pos_start
+      pos_nummove <- 0
+      piece_was_captured <- FALSE
+      path <- NULL
       path <- c(path, pos_current)
-    }
-    
-    dfpaths <- data_frame(from = head(path, -1),
-                          to = tail(path, -1))
-    
-    dfpaths
-    
-  })
+      
+      while (!piece_was_captured) {
+        dfmvex_aux <- dfmvex %>%
+          filter(from == pos_current | to == pos_current,
+                 number_move > pos_nummove) %>% 
+          head(1)
+        
+        if (nrow(dfmvex_aux) == 0) break
+        if (dfmvex_aux$to == pos_current) break
+        
+        pos_current <- dfmvex_aux$to
+        pos_nummove <- dfmvex_aux$number_move
+        
+        path <- c(path, pos_current)
+      }
+      
+      dfpaths <- data_frame(from = head(path, -1),
+                            to = tail(path, -1))
+      
+      dfpaths
+      
+    })
+  
+  dfresponse <- dfresponse %>% 
+    left_join(dfboard %>% rename(from = cell, x.from = x, y.from = y), by = "from") %>% 
+    left_join(dfboard %>% rename(to = cell, x.to = x, y.to = y) %>% select(-cc, -col, -row), by = "to") %>% 
+    mutate(curve = ifelse((x.from - x.to) < 0, TRUE, FALSE))
+  
+  dfresponse
+  
+  
+}
 
-
-
-dfpaths <- dfpaths %>% 
-  left_join(dfboard %>% rename(from = cell), by = "from") %>% 
-  left_join(dfboard %>% rename(to = cell) %>% select(-cc), by = "to") %>% 
-  mutate(curve = ifelse((x.x - x.y) < 0, TRUE, FALSE))
-
+dfpaths_g1 <- get_paths_from_position("g1") %>% sample_frac(0.2)
 
 ggplot() +
   geom_tile(data = dfboard, aes(x, y, fill = cc)) +
-  geom_curve(data = dfpaths %>% filter(curve),
-             aes(x = x.x, y = y.x, xend = x.y, yend = y.y), color = "white", alpha = 0.2, curvature = -.3,
-             arrow = arrow(length = unit(0.25,"cm")), position =  position_jitter(w = 0.25, h = 0.25)) +
-  geom_curve(data = dfpaths %>% filter(!curve),
-             aes(x = x.x, y = y.x, xend = x.y, yend = y.y), color = "white", alpha = 0.2, curvature = .3,
-             arrow = arrow(length = unit(0.25,"cm")), position =  position_jitter(w = 0.25, h = 0.25)) +
+  geom_curve(data = dfpaths_g1 %>% filter(curve),
+             aes(x = x.from, y = y.from, xend = x.to, yend = y.to),
+             color = "white", alpha = 0.1, curvature = 0.75, angle = -45,
+             position =  position_jitter(width = 0.25, height = 0.25)) +
+  geom_curve(data = dfpaths_g1 %>% filter(!curve),
+             aes(x = x.from, y = y.from, xend = x.to, yend = y.to),
+             color = "white", alpha = 0.1, curvature = 0.75, angle = -45,
+             position =  position_jitter(width = 0.25, height = 0.25)) +
   scale_fill_manual(values =  c("gray10", "gray20")) +
   coord_equal() + 
   ggthemes::theme_map() +
   theme(legend.position = "none")
 
+
+dfpaths_d1 <- get_paths_from_position("d1") %>% sample_frac(0.2)
+
 ggplot() +
   geom_tile(data = dfboard, aes(x, y, fill = cc)) +
-  geom_point(data = dfpaths, aes(x.x, y.x), position =  position_jitter(w = 0.25, h = 0.25),
-             color = "white", alpha = 0.1) +
-  geom_curve(data = dfpaths %>% filter(curve),
-             aes(x = x.x, y = y.x, xend = x.y, yend = y.y), color = "white", alpha = 0.2, curvature = -.3,
-             position =  position_jitter(w = 0.25, h = 0.25)) +
-  geom_curve(data = dfpaths %>% filter(!curve),
-             aes(x = x.x, y = y.x, xend = x.y, yend = y.y), color = "white", alpha = 0.2, curvature = .3,
-             position =  position_jitter(w = 0.25, h = 0.25)) +
+  geom_curve(data = dfpaths_g1 %>% filter(curve),
+             aes(x = x.from, y = y.from, xend = x.to, yend = y.to),
+             color = "white", alpha = 0.1, curvature = 0.75, angle = -45,
+             position =  position_jitter(width = 0.25, height = 0.25)) +
+  geom_curve(data = dfpaths_g1 %>% filter(!curve),
+             aes(x = x.from, y = y.from, xend = x.to, yend = y.to),
+             color = "white", alpha = 0.1, curvature = 0.75, angle = -45,
+             position =  position_jitter(width = 0.25, height = 0.25)) +
   scale_fill_manual(values =  c("gray10", "gray20")) +
   coord_equal() + 
   ggthemes::theme_map() +
   theme(legend.position = "none")
-
-
-
 
