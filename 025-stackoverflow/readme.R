bdfb2dcf28ef09b53c72c0c067c707ad01e3cba8
Joshua <jbkunst@gmail.com>
2015-11-02 18:20:58 -0300

    updating this post

--- 025-stackoverflow/readme.R
+++ 025-stackoverflow/readme.R
@@ -15,24 +15,34 @@ library("lubridate")
 SITE <- "stackoverflow"
 TAG <- "r"
 KEY <- "ifpYG3FCatEyPyX8AqkVCA(("
-NPAGES <- 5000 # Requests  
 API_URL <- "https://api.stackexchange.com/2.2/questions"
 NTAGS <- 50
 
 
 ##### Question List
-qlist <- llply(seq(NPAGES), function(p) { # p <- 5000
-  data <- API_URL %>% 
-    GET(query = list(site = SITE, tagged = TAG, page = p, key = KEY,
-                     sort = "creation", pagesize = 100, order = "desc")) %>% 
-    content()
+question_donwload <- function(verbose = FALSE){
+  qlist <- list()
+  carry_on <- TRUE
+  actual_page <- 1
   
-  if (p %% 100 == 0) message("page: ",p ," | quota remaining: ", data$quota_remaining)
+  while (carry_on) {
+    data <- API_URL %>% 
+      GET(query = list(site = SITE, tagged = TAG, page = actual_page, key = KEY,
+                       sort = "creation", pagesize = 100, order = "desc")) %>% 
+      content()
+    
+    if (data$has_more) carry_on <- data$has_more
+    if (actual_page %% 100 == 0) message("page: ",actual_page ," | quota remaining: ", data$quota_remaining)
+    
+    qlist[[actual_page]] <- data$items
+    actual_page <- actual_page + 1
+    
+  }
   
-  data$items
-}, .progress = "win") 
-qlist <- unlist(qlist, recursive = FALSE)
-
+  qlist <- unlist(qlist, recursive = FALSE)
+  
+}
+# qlist <- question_donwload()
 # saveRDS(qlist, "qlist.Rds")
 qlist <- readRDS("qlist.Rds")
 
@@ -56,6 +66,8 @@ df_qst <- ldply(qlist, function(x){
 
 df_qst <- tbl_df(df_qst)
 
+rm(x, namestoselc)
+
 df_qst <- df_qst %>% 
   mutate(creation_date = as.POSIXct(creation_date, origin = "1970-01-01"),
          last_activity_date = as.POSIXct(last_activity_date, origin = "1970-01-01"),
@@ -71,12 +83,23 @@ df_qtag <- ldply(qlist, function(x){
   # x <- sample(qlist, size = 1)[[1]]
   tags <- x$tags %>% unlist()
   if (length(tags) > 1) {
-    data_frame(question_tag = setdiff(tags, TAG),
+    data_frame(question_tag = tags,
                question_id = x$question_id)  
   }
 }, .progress = "win")
 
 df_qtag <- tbl_df(df_qtag)
+df_qtag <- df_qtag %>% filter(question_tag != "r")
+
+df_qtag_moretags <- inner_join(df_qtag,
+                               df_qtag %>% count(question_id) %>% filter(n > 1) %>% select(question_id),
+                               by = "question_id")
+
+
+df_qtag_moretags %>% count(question_tag) %>% arrange(desc(question_tag))
+
+
+
 
 
 df_qtag_top <- df_qtag %>% 
