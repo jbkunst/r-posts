c6482a7089eca52489ac6428e80bc823f9edf2be
Joshua <jbkunst@gmail.com>
2016-03-10 01:59:16 -0300

    the seed

---
+++ 038-elo-glicko/readme.R
@@ -0,0 +1,15 @@
+#' ---
+#' title: ""
+#' author: "Joshua Kunst"
+#' output:
+#'  html_document:
+#'    toc: true
+#'    keep_md: yes
+#' ---
+
+#+ echo=FALSE, message=FALSE, warning=FALSE
+#### setup ws packages ####
+rm(list = ls())
+knitr::opts_chunk$set(message=FALSE, warning=FALSE)
+
+#'
