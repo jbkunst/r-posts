93ebb4356d8cb824f692356040eec5b6e1f3e6e6
Joshua <jbkunst@gmail.com>
2016-04-05 04:12:57 -0300

    zzzzz

--- 047-52vis-2016-13/readme.R
+++ 047-52vis-2016-13/readme.R
@@ -12,12 +12,19 @@
 rm(list = ls())
 knitr::opts_chunk$set(message = FALSE, warning = FALSE,
                       fig.showtext = TRUE, dev = "CairoPNG")
-library(ggplot2)
-library(ggalt)
-library(ggthemes)
-library(readxl)
-library(dplyr)
-library(grid)
+library("ggplot2")
+library("ggalt")
+library("ggthemes")
+library("readxl")
+library("dplyr")
+library("viridis")
+library("stringr")
+library("grid")
+library("lubridate")
+library("gridExtra")
+library("showtext")
+library("jbkmisc")
+blog_setup()
 
 URL1 <- "http://www.faa.gov/uas/media/UAS_Sightings_report_21Aug-31Jan.xlsx"
 URL2 <- "http://www.faa.gov/uas/media/UASEventsNov2014-Aug2015.xls"
@@ -34,69 +41,101 @@ xl2 <- read_excel(fil2)
 drones <- setNames(bind_rows(xl2[,1:3],
                              xl1[,c(1,3,4)]), 
                    c("ts", "city", "state"))
-drones <- mutate(drones, 
-                 year = format(ts, "%Y"), 
-                 year_mon = format(ts, "%Y%m"), 
-                 ymd = as.Date(ts), 
-                 yw = format(ts, "%Y%V"))
 
-by_week <- mutate(count(drones, yw), wk = as.Date(sprintf("%s1", yw), "%Y%U%u") - 7)
-by_week <- arrange(filter(by_week, wk >= as.Date("2014-11-10")), wk)
+drones 
 
-gg <- ggplot(by_week, aes(wk, n))
-gg <- gg + geom_bar(stat = "identity", fill = "#937206")
-gg <- gg + annotate("text", by_week$wk[1], 49, label = "# reports", 
-                    hjust = 0, vjust = 1, family = "Cabin-Italic", size = 3)
-gg <- gg + scale_x_date(expand = c(0,0))
-gg <- gg + scale_y_continuous(expand = c(0,0))
-gg <- gg + labs(y = NULL,
-                title = "Weekly U.S. UAS (drone) sightings",
-                subtitle = "As reported to the Federal Aviation Administration",
-                caption = "Data from: http://www.faa.gov/uas/law_enforcement/uas_sighting_reports/")
-gg <- gg + theme_minimal()
-gg <- gg + theme(axis.title.x = element_text(margin = margin(t = -6)))
-gg
+try(x <- Sys.setlocale("LC_TIME", "en_US.UTF-8"))
+try(x <- Sys.setlocale("LC_TIME", "English"))
+
+drones <- drones %>% 
+  mutate(year = format(ts, "%Y"),
+         year_mon = format(ts, "%Y%m"),
+         ymd = as.Date(ts), 
+         yw = format(ts, "%Y%V"),
+         hour = as.numeric(format(ts, "%H")),
+         day = wday(ts, label = TRUE, abbr = FALSE)) %>% 
+  filter(!is.na(day))
+
+drones$hr2 <- drones$ts %>% format("%H:%M:%S") %>% stringr::str_pad(8, side = "left", pad = "0") %>% hms()
+drones 
 
 
-
-library("rvest")
-library("stringr")
-library("purrr")
-library("lubridate")
-library("viridis")
-
-str_locate2 <- function(string, pattern) {
-  str_locate(string, pattern)[, 1]
+hoursfmt <- function(x = c(1:24)) {
+  x %>% 
+    str_pad(2, side = "left", pad = "0") %>% 
+    paste0(":00")
 }
 
-url <- "https://en.wikipedia.org/wiki/List_of_U.S._states_and_territories_by_population"
+
+viridis(10)
+col <- "#3E4A89"
+col <- "#35B779"
 
 
-statespop <- read_html(url) %>% 
-  html_table(fill = TRUE) %>% 
-  .[[1]] %>% 
-  select(c(3, 4)) %>% 
-  tbl_df() %>% 
-  setNames(c("state", "pop")) %>% 
-  mutate(state = str_sub(state, 2),
-         state = ifelse(str_detect(state, " Dakota"), "North Dakota", state),
-         state = str_trim(state),
-         pop = str_replace_all(pop, "\\,", ""),
-         pop = as.numeric(pop))
-  
-
-dronesym <- drones %>%
-  filter(!is.na(ymd), state != "CANADA") %>% 
-  mutate(year_mon = paste0(year_mon, "01"),
-         year_mon = ymd(year_mon)) %>% 
-  group_by(state, year_mon) %>% 
-  summarise(count = n()) %>% 
-  ungroup() %>% 
-  left_join(statespop, by = "state") %>% 
-  mutate(count2 = count/pop * 1000000)
+theme_set(theme_jbk() +
+            theme(plot.background=element_rect(fill="#2D2D2D", colour="#2D2D2D"),
+                  text = element_text(colour = "gray60"))
+          )
 
 
-ggplot(dronesym) + 
-  geom_tile(aes(year_mon, state, fill = count2)) + 
+gg1 <- drones %>% 
+  group_by(hour) %>% 
+  summarise(n = n()) %>% 
+  ggplot() +
+  geom_bar(aes(hour, n), fill = col, stat = "identity") +
+  scale_x_continuous(labels = hoursfmt, breaks = c(0, 6, 12, 18, 23)) +
+  xlab(NULL) + ylab(NULL) +  
+  theme(panel.grid = element_blank(),
+        axis.text.y = element_blank(),
+        legend.position = "none")
+gg1
+
+gg2 <- ggplot(drones, aes(x = ts, y = hour)) + 
+  geom_point(alpha = 0.6) + 
+  geom_smooth(color = col)
+
+
+gg2 <- drones %>% 
+  count(hour) %>% 
+  mutate(ymin = 0.5, ymax = 1) %>% 
+  rbind(data_frame(hour = 24, n = 0, ymin = 0.5, ymax = 0.5)) %>% 
+  ggplot() +
+  geom_linerange(aes(x = hour,  color = n, ymin = ymin, ymax = ymax), size = 7) +
+  scale_x_continuous(labels = hoursfmt, breaks = c(0, 3, 6, 9, 12, 15, 18, 21)) +
+  scale_color_viridis() + 
+  coord_polar() + 
+  xlab(NULL) + ylim(-1, 1) +
+  theme(panel.grid = element_blank(),
+        axis.text.y = element_blank(),
+        legend.position = "none")
+
+gg3 <- drones %>% 
+  group_by(day, hour) %>% 
+  summarise(n = n()) %>% 
+  ggplot() +
+  geom_tile(aes(hour, day, fill = n)) +
   scale_fill_viridis() +
-  theme_minimal()
+  scale_x_continuous(labels = hoursfmt, breaks = c(0, 6, 12, 18, 23)) +
+  xlab(NULL) + ylab(NULL) + 
+  theme(panel.grid = element_blank(),
+        axis.text = element_blank(),
+        legend.position = "none")
+gg3
+
+gg4 <- drones %>% 
+  group_by(day) %>% 
+  summarise(n = n()) %>% 
+  ggplot() +
+  geom_bar(aes(day, n), fill = col, stat = "identity") +
+  coord_flip() + xlab(NULL) + ylab(NULL) +
+  theme(panel.grid = element_blank(),
+        axis.text.x = element_blank(),
+        legend.position = "none")
+gg4
+
+
+
+grid.arrange(gg1, gg2, gg3, gg4, layout_matrix = rbind(c(1,1,2),c(1,1,2),c(3,3,4), c(3,3,4)))
+
+grid.arrange(gg1, gg2, gg3, gg4 + scale_y_reverse(), layout_matrix = rbind(c(2,1,1),c(4,3,3), c(4,3,3)))
+
--- 047-52vis-2016-13/readme.md
+++ 047-52vis-2016-13/readme.md
@@ -9,11 +9,54 @@ Joshua Kunst
 ## DEFINEDNAME: 00 00 00 06 0b 00 00 00 01 00 00 00 00 00 00 6f 77 73 73 76 72 3b 00 00 00 00 fc 02 00 00 05 00
 ```
 
-![](readme_files/figure-html/unnamed-chunk-1-1.png)![](readme_files/figure-html/unnamed-chunk-1-2.png)
+```
+## Source: local data frame [1,347 x 3]
+## 
+##                     ts            city         state
+##                 (time)           (chr)         (chr)
+## 1  2014-11-13 15:33:59     Viginia Key       Florida
+## 2  2014-11-13 16:44:00       Las Vegas        Nevada
+## 3  2014-11-14 13:54:59     Los Angeles    California
+## 4  2014-11-14 16:09:00         Norfolk      Virginia
+## 5  2014-11-14 18:58:00      Scottsdale       Arizona
+## 6  2014-11-15 14:29:59      Tuscaloosa       Alabama
+## 7  2014-11-15 19:09:59 Fort Lauderdale       Florida
+## 8  2014-11-18 17:10:00        New York      New York
+## 9  2014-11-19 13:57:00        New York      New York
+## 10 2014-11-19 16:17:00          Boston Massachusetts
+## ..                 ...             ...           ...
+```
+
+```
+## Source: local data frame [1,346 x 10]
+## 
+##                     ts            city         state  year year_mon
+##                 (time)           (chr)         (chr) (chr)    (chr)
+## 1  2014-11-13 15:33:59     Viginia Key       Florida  2014   201411
+## 2  2014-11-13 16:44:00       Las Vegas        Nevada  2014   201411
+## 3  2014-11-14 13:54:59     Los Angeles    California  2014   201411
+## 4  2014-11-14 16:09:00         Norfolk      Virginia  2014   201411
+## 5  2014-11-14 18:58:00      Scottsdale       Arizona  2014   201411
+## 6  2014-11-15 14:29:59      Tuscaloosa       Alabama  2014   201411
+## 7  2014-11-15 19:09:59 Fort Lauderdale       Florida  2014   201411
+## 8  2014-11-18 17:10:00        New York      New York  2014   201411
+## 9  2014-11-19 13:57:00        New York      New York  2014   201411
+## 10 2014-11-19 16:17:00          Boston Massachusetts  2014   201411
+## ..                 ...             ...           ...   ...      ...
+## Variables not shown: ymd (date), yw (chr), hour (dbl), day (fctr), hr2
+##   (dbl)
+```
+
+```
+##  [1] "#440154FF" "#482878FF" "#3E4A89FF" "#31688EFF" "#26828EFF"
+##  [6] "#1F9E89FF" "#35B779FF" "#6DCD59FF" "#B4DE2CFF" "#FDE725FF"
+```
+
+![](readme_files/figure-html/unnamed-chunk-1-1.png)![](readme_files/figure-html/unnamed-chunk-1-2.png)![](readme_files/figure-html/unnamed-chunk-1-3.png)![](readme_files/figure-html/unnamed-chunk-1-4.png)![](readme_files/figure-html/unnamed-chunk-1-5.png)
 
 
 ---
 title: "readme.R"
-author: "jkunst"
-date: "Thu Mar 31 17:59:58 2016"
+author: "Joshua K"
+date: "Tue Apr 05 04:00:37 2016"
 ---
