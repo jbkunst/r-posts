4349046b191ed04bd38d6f0c1b1a12d44748e4d2
Joshua <jbkunst@gmail.com>
2016-05-11 17:56:18 -0300

    start mdsl + validation

--- 050-caret-demo/readme.R
+++ 050-caret-demo/readme.R
@@ -83,9 +83,10 @@ rfWithFilter <- sbf(x, y, sbfControl = filterCtrl)
 rfWithFilter
 
 #'
-#' # Models
+#' # Models & Validation
 library(caret)
 library(purrr)
+library(Metrics)
 
 #' Models whichs support regression:
 
@@ -94,52 +95,57 @@ map(getModelInfo(), "type") %>%
     any(x == "Regression")
   }) %>% 
   {.[.]} %>% 
-  names()
+  names() %>% 
+  sort()
 
-mods_names <- c("neuralnet", "glmboost", "kknn", "ctree", "gbm", "lasso", "gamboost", "xgbTree")
+mods_names <- c("earth", "ctree", "rpart", "rf", "gbm", "nnet")
 
 seqnvars <- seq(nrow(rrfimp))
 
+act <- dftest$count
 
-map(seqnvars, function(nvars){
+
+dfbench <- map(seqnvars[-1], function(nvars){
+  # nvars <- 5
   
   dftrainsub <- dftrain[, seq(1, 1 + nvars)]
   
   mods_fit <- map(mods_names, function(modname){
     
-    message(nvars, " : ", modname)
+    # modname <- "earth"
     
-    train(formula, data = dftrain, method = modname, trControl = trainControl(method = "none")) 
+    message("variables: ", nvars, " | modelo: ", modname)
+    
+    train(y = dftrain$count, x = dftrainsub, method = modname) 
     
   })
   
+  predictions <- predict(mods_fit, newdata = dftest)
+  
+  mses <- map_dbl(predictions, function(x){
+    mse(x, act)
+  })
+  
+  data_frame(mod = mods_names, mse = mses, nvar = nvars)
+  
 })
 
+dfbench2 <- map_df(dfbench, identity) %>% 
+  mutate(nmvar = rep(rrfimp$variable, each = length(mods_names)),
+         nmvar = factor(nmvar, rrfimp$variable))
 
 
-#'
-#' # Validation
-library(Metrics)
+library(ggplot2)
 
-mape <- function(actual, predicted) {
-  mean(abs((actual - predicted)/actual))
-}
+ggplot(dfbench2) +
+  geom_line(aes(nmvar, mse, group = mod, color = mod)) + 
+  coord_flip() + 
+  scale_y_reverse()
 
-predictions <- predict(mods_fit, newdata = dftest)
+ggplot(dfbench2) +
+  geom_line(aes(nmvar, mse, group = mod, color = mod)) +
+  # theme(axis.text.x = element_text(angle = 90)) + 
+  scale_y_log10()
 
-names(predictions) <- mods_names
-
-map(predictions, head)
-
-act <- dftest$count
-
-resulst <- map_df(predictions, function(x){
-  data_frame(
-    mse = mse(act, x),
-    mae = mae(act, x),
-    mape = mape(act, x)
-  )
-}, .id = "model")
-
-resulst
+dfbench2 %>% filter(nmvar == "season")
 
