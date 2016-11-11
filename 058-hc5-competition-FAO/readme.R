ea3fc715a2d238957812fc1024aaeb1e4e2815a6
Joshua <jbkunst@gmail.com>
2016-11-11 15:52:04 -0300

    hc5 competition

---
+++ 058-hc5-competition-FAO/readme.R
@@ -0,0 +1,17 @@
+#' ---
+#' title: "Highcharts 5 Competition"
+#' author: "Joshua Kunst"
+#' output:
+#'  html_document:
+#'    toc: true
+#'    keep_md: yes
+#' ---
+
+#+ echo=FALSE, message=FALSE, warning=FALSE
+#### setup ws packages ####
+rm(list = ls())
+knitr::opts_chunk$set(message = FALSE, warning = FALSE)
+
+#' Load Packages
+library(tidyverse)
+library(highcharter)
