f2645a4e2d20938facbe25b06e2eb57c31150d77
jbkunst <jbkunst@gmail.com>
2016-11-29 00:05:20 -0300

    almost done

--- 057-weather-chart-tufte/readme.R
+++ 057-weather-chart-tufte/readme.R
@@ -10,8 +10,7 @@
 #+ echo=FALSE, message=FALSE, warning=FALSE
 #### setup ws packages ####
 rm(list = ls())
-knitr::opts_chunk$set(message = FALSE, warning = FALSE,
-                      fig.showtext = TRUE, dev = "CairoPNG")
+knitr::opts_chunk$set(message = FALSE, warning = FALSE)
 
 #'
 library(tidyverse)
@@ -29,11 +28,12 @@ file <- "new-york_ny.csv" # "san-francisco_ca.csv"
 url_file <- file.path(url_base, file)
 
 data <- read_csv(url_file)
-data
+glimpse(data)
 
 data <- mutate(data, dt = datetime_to_timestamp(date))
 
-options(highcharter.theme = hc_theme_smpl())
+options(highcharter.theme = hc_theme_smpl(),
+        highcharter.debug = TRUE)
 
 
 #' ## Setup
@@ -47,7 +47,7 @@ hc <- highchart() %>%
            showLastLabel = FALSE) %>% 
   # setting tooltip
   hc_tooltip(shared = TRUE,
-             seHTML = TRUE,
+             useHTML = TRUE,
              headerFormat = as.character(tags$small("{point.x: %b %d}", tags$br()))) %>% 
   hc_plotOptions(series = list(borderWidth = 0, pointWidth = 4))
 
@@ -116,24 +116,27 @@ axis <- create_yaxis(
   startOnTick = FALSE)
 
 #' Manually add titles (I know this can be more elegant)
-# axis[[1]]$title <- list(text = "Temperature")
-# axis[[2]]$title <- list(text = "Precipitation")
-# 
-# hc <- hc_yAxis_multiples(hc, axis)  
-# 
-# hc
+axis[[1]]$title <- list(text = "Temperature")
+axis[[1]]$labels <- list(format = "{value}°F")
+axis[[2]]$title <- list(text = "Precipitation")
+
+hc <- hc_yAxis_multiples(hc, axis)
+
+hc
 
 #' Te 2 axis are ready, now just add the data. Here we will add 12 series 
 #' -one for each month- but we want to asociate 1 legend for all these 12 
 #' series, so we need to use `id` and `linkedTo` parameters and obviously
 #' add this data to the second axis.
 
-# hc <- hc %>% 
-#   hc_add_series(data, type = "area", hcaes(dt, precip_value, group = month),
-#                 name = "Precipitation", color = "skyblue", yAxis = 1, 
-#                 id = c("p", rep(NA, 11)), linkedTo = c(NA, rep("p", 11)))
-# 
-# hc
+precipdata <- select(data, dt, precip_value, month)
+
+hc <- hc %>%
+  hc_add_series(precipdata, type = "area", hcaes(dt, precip_value, group = month),
+                name = "Precipitation", color = "skyblue", yAxis = 1,
+                id = c("p", rep(NA, 11)), linkedTo = c(NA, rep("p", 11)))
+
+hc
 
 #' ## Some final touches
 #' 
@@ -159,9 +162,20 @@ title <- paste0(city, ", ", state)
 hc <- hc %>%
   hc_title(text = title)
 
-#' ### Records
+#' ### Normal precipitations
+precipnormal <- select(data, dt, precip_normal, month) %>% 
+  group_by(month) %>% 
+  filter(row_number() %in% c(1, n())) %>%
+  ungroup() %>% 
+  fill(precip_normal)
 
-annotations <- data %>%
+hc <- hc %>% 
+  hc_add_series(precipnormal, "line", hcaes(x = dt, y = precip_normal, group = month),
+                name = "Normal Precipitation", color = "#EBEAE2", yAxis = 1,
+                id = c("np", rep(NA, 11)), linkedTo = c(NA, rep("np", 11)))
+  
+#' ### Records
+records <- data %>%
   select(dt, temp_rec_high, temp_rec_low) %>% 
   filter(temp_rec_high != "NULL" | temp_rec_low != "NULL") %>% 
   dmap_if(is.character, str_extract, "\\d+") %>% 
@@ -169,27 +183,23 @@ annotations <- data %>%
   gather(type, value, -dt) %>% 
   filter(!is.na(value)) %>% 
   mutate(type = str_replace(type, "temp_rec_", ""),
-         xValue = dt,
-         yValue = value)
-  
+         type = paste("This year record", type))
 
-annotations$title <- map3(annotations$type, annotations$value, annotations$type, function(x, y, z){
-  
-  sign <- ifelse(z == "high", -1, 1)
-  list(text = paste(x, y), style = list(fontSize = 10),  y = 40 * sign)
+records
 
-})
-
-annotations$shape <- map(annotations$type, function(t){
-  sign <- ifelse(t == "high", -1, 1)
-  sign <- 1
-  list(type = "path", params = list(d = list('M', 10, 0, 'V', 30 * sign)))
-})
-
-hc %>% 
-  hc_add_annotations(annotations)
+pointsyles <- list(
+  symbol = "circle",
+  lineWidth= 1,
+  radius= 4,
+  fillColor= "#FFFFFF",
+  lineColor= NULL
+)
 
 
+hc <- hc %>% 
+  hc_add_series(records, "point", hcaes(x = dt, y = value, group = type),
+                marker = pointsyles)
 
 #' ## Volia
 hc
+
