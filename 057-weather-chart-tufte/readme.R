951d8f3f6488f1b0bb0920c1d5ae63c72e723d98
Joshua <jbkunst@gmail.com>
2016-12-01 18:16:07 -0300

    :arrow_up:

--- 057-weather-chart-tufte/readme.R
+++ 057-weather-chart-tufte/readme.R
@@ -21,7 +21,7 @@ library(forcats)
 
 #' ## Data
 #' 
-#' Lorem lorem
+#' If you search a little in the devTools in the 
 #' 
 url_base <- "http://graphics8.nytimes.com/newsgraphics/2016/01/01/weather/assets"
 file <- "new-york_ny.csv" # "san-francisco_ca.csv"
@@ -32,8 +32,7 @@ glimpse(data)
 
 data <- mutate(data, dt = datetime_to_timestamp(date))
 
-options(highcharter.theme = hc_theme_smpl(),
-        highcharter.debug = TRUE)
+options(highcharter.theme = hc_theme_smpl())
 
 
 #' ## Setup
@@ -41,41 +40,32 @@ options(highcharter.theme = hc_theme_smpl(),
 #' Lorem lorem setup
 #' 
 hc <- highchart() %>%
-  # axis setup
-  hc_xAxis(type = "datetime",
-           dateTimeLabelFormats = list(month = "%B"),
-           showLastLabel = FALSE) %>% 
-  # setting tooltip
-  hc_tooltip(shared = TRUE,
-             useHTML = TRUE,
+  hc_xAxis(type = "datetime", showLastLabel = FALSE,
+           dateTimeLabelFormats = list(month = "%B")) %>% 
+  hc_tooltip(shared = TRUE, useHTML = TRUE,
              headerFormat = as.character(tags$small("{point.x: %b %d}", tags$br()))) %>% 
   hc_plotOptions(series = list(borderWidth = 0, pointWidth = 4))
 
 hc
 
-#' ## Adding temp data
+#' _No data to display_. All acording to the plan XD.
 #' 
-#'  Some fun wrangling the data, some gather, some spread.
+#' ## Temperature Data
 #' 
+#' We'll select the temperature columns from the data and do some wrangling,
+#' gather, spread, separate and recodes to get a nice tidy data frame.
 #' 
 dtempgather <- data %>% 
-select(dt, starts_with("temp")) %>% 
+  select(dt, starts_with("temp")) %>% 
   select(-temp_rec_high, -temp_rec_low) %>% 
   rename(temp_actual_max = temp_max,
          temp_actual_min = temp_min) %>% 
   gather(key, value, -dt) %>% 
   mutate(key = str_replace(key, "temp_", "")) 
 
-dtempgather
-
 dtempspread <- dtempgather %>% 
   separate(key, c("serie", "type"), sep = "_") %>% 
   spread(type, value)
-
-dtempspread
-
-#' Finaly some fixes
-#' 
 
 temps <- dtempspread %>% 
   mutate(serie = factor(serie, levels = c("rec", "avg", "actual")),
@@ -83,7 +73,7 @@ temps <- dtempspread %>%
 
 temps
 
-#' So, why thiz codez? Just for get a tidy data for easy charting.
+#' Now whe can add this data to the _highchart_ object using `hc_add_series`:
 
 hc <- hc %>% 
   hc_add_series(temps, type = "columnrange",
@@ -92,7 +82,13 @@ hc <- hc %>%
 
 hc
 
-#' ## Records
+#' A really similar chart of what we want!
+#' 
+#' The chart show records of temprerature. So 
+#' we need to filter the days with temperature records using the columns
+#' `temp_rec_high` and `temp_rec_low`, then some gathers and tweaks. Then
+#' set some options to show the points, like use fill color and some longer
+#' radius.
 records <- data %>%
   select(dt, temp_rec_high, temp_rec_low) %>% 
   filter(temp_rec_high != "NULL" | temp_rec_low != "NULL") %>% 
@@ -120,9 +116,13 @@ hc <- hc %>%
 
 hc
 
-#' ## Adding other data in other axis
+#' We're good.
 #' 
-#' First create a list with 2 axis and using the  `create_yaxis` helper
+#' ## Precipitation Data
+#' 
+#' A nice feture of the NYTs app is and the chart is show the precipitaion by
+#' month. This data is in other axis. So we need to create a list with 2 axis 
+#' using the `create_yaxis` helper and the adding this axis to the chart.
 axis <- create_yaxis(
   naxis = 2,
   heights = c(3,1),
@@ -131,7 +131,9 @@ axis <- create_yaxis(
   showLastLabel = FALSE,
   startOnTick = FALSE)
 
-#' Manually add titles (I know this can be more elegant)
+#' 
+#' Manually add titles (I know this can be more elegant) adn options.
+#' 
 axis[[1]]$title <- list(text = "Temperature")
 axis[[1]]$labels <- list(format = "{value}°F")
 
@@ -142,23 +144,24 @@ hc <- hc_yAxis_multiples(hc, axis)
 
 hc
 
-#' Te 2 axis are ready, now just add the data. Here we will add 12 series 
+#' The 2 axis are ready, now we need add the data. We will add 12 series 
 #' -one for each month- but we want to asociate 1 legend for all these 12 
-#' series, so we need to use `id` and `linkedTo` parameters and obviously
-#' add this data to the second axis.
+#' series, so we need to use `id` and `linkedTo` parameters and obviously. 
+#' That's why the `id` will be a `'p'` for the firt element and then `r NA`
+#' to the other 11. And then linked this 11 to the first series (`id = 'p'`).
 
 precip <- select(data, dt, precip_value, month)
 
 hc <- hc %>%
   hc_add_series(precip, type = "area", hcaes(dt, precip_value, group = month),
-                name = "Precipitation", color = "skyblue", yAxis = 1,
+                name = "Precipitation", color = "#008ED0", lineWidth = 1,
+                yAxis = 1, fillColor = "#EBEAE2", 
                 id = c("p", rep(NA, 11)), linkedTo = c(NA, rep("p", 11)))
 
-hc
-
-#' Normal precipitations
+#' The same way we'll add the normal precipitations by month.
 #' 
-precipnormal <- select(data, dt, precip_normal, month) %>% 
+precipnormal <- data %>% 
+  select(dt, precip_normal, month) %>% 
   group_by(month) %>% 
   filter(row_number() %in% c(1, n())) %>%
   ungroup() %>% 
@@ -166,12 +169,22 @@ precipnormal <- select(data, dt, precip_normal, month) %>%
 
 hc <- hc %>% 
   hc_add_series(precipnormal, "line", hcaes(x = dt, y = precip_normal, group = month),
-                name = "Normal Precipitation", color = "#C8B8B9", yAxis = 1,
+                name = "Normal Precipitation", color = "#008ED0", yAxis = 1,
                 id = c("np", rep(NA, 11)), linkedTo = c(NA, rep("np", 11)),
                 lineWidth = 1)
   
 
-
+#' 
 #' ## Volia
+#' 
+#' Curious how the chart looks? Me too! Crossing fingers...
+#' 
 hc
 
+
+#' So, with R you can create a press style chart with some wrangling and charting. 
+#' Now with a little of love we can make the code resuable to make a shiny app.
+#' 
+#' <iframe src="https://jbkunst.shinyapps.io/shiny-nyt-temp/" width="100%" height="750px"
+#' style="border:none;"></iframe>
+#' 
