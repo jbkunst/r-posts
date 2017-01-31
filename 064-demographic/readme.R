3a33ebcff6dc8b9fafa7654713e133eebcaf675d
Joshua <jbkunst@gmail.com>
2017-01-31 18:33:43 -0300

    asdf

--- 064-demographic/readme.R
+++ 064-demographic/readme.R
@@ -13,49 +13,48 @@ knitr::opts_chunk$set(message = FALSE, warning = FALSE, fig.cap = "")
 
 #  ------------------------------------------------------------------------
 library(tidyverse)
+library(highcharter)
 library(idbr)
-library(countrycode)
-library(highcharter) 
-library(httr)
 library(viridisLite)
+library(forcats)
 library(stringr)
 
 options(highcharter.theme = hc_theme_smpl())
-idbr::idb_api_key("35f116582d5a89d11a47c7ffbfc2ba309133f09d")
+idb_api_key("35f116582d5a89d11a47c7ffbfc2ba309133f09d")
 
-data(countrycode_data)
-countrycode_data <- tbl_df(countrycode_data)
-countrycode_data
+# data(countrycode_data)
+# countrycode_data <- tbl_df(countrycode_data)
+# countrycode_data
+# fips <- countrycode_data$fips104
+# fips <- na.omit(fips)
+# fips_path <- "data/fips/fips_valid.rds"
+# if(!file.exists(fips_path)) {
+#   fips_valid <- map_chr(sample(fips), function(x){ # x <- "AL"
+#     message(x)
+#     try({ idb1(x, 1990, variables = c("AGE", "POP")); return(x) })
+#   })
+#   fips_valid <- fips_valid[nchar(fips_valid) == 2]
+#   saveRDS(fips_valid, fips_path)
+# } else {
+#   fips_valid <- readRDS(fips_path)
+# }
+# 
+# countrycode_data <- filter(countrycode_data, fips104 %in% fips_valid)
+# countrycode_data
+# 
+# 
+# set.seed(9876543)
 
-fips <- countrycode_data$fips104
-fips <- na.omit(fips)
+fip <- "US"
+yrs <- 1990:2050
 
-fips_valid <- map_chr(sample(fips), function(x){ # x <- "AL"
-  message(x)
-  try({
-    idb1(x, 1990, variables = c("AGE", "POP"))
-    return(x)
-  })
+system.time({
+  data <- idb1(fip, yrs, variables = c("AGE", "POP", "NAME"), sex = "both")  
 })
 
-countrycode_data <- filter(countrycode_data, fips104 %in% fips_valid)
-countrycode_data
-
-
-
-set.seed(9876543)
-countrycode_data %>% 
-  sample_n(1) %>% 
-  print() %>% 
-  .[["fips104"]] -> fip
-
-yrs <- 1990:2050
-brks <- seq(0, 100, length.out = 10 + 1)
-
-
-data <- idb1(fip, yrs, variables = c("AGE", "POP", "NAME"))
 data
 
+brks <- seq(0, 100, length.out = 10 + 1)
 
 datag <- data %>% 
   mutate(agec = cut(AGE, breaks = brks, include.lowest = TRUE)) %>% 
@@ -63,17 +62,22 @@ datag <- data %>%
   summarise(pop = sum(POP),
             age_median = round(median(AGE))) %>% 
   ungroup() %>% 
-  mutate(birth_year = year - age_median) %>% 
+  arrange(desc(age_median)) %>% 
+  mutate(birth_year = year - age_median,
+         agec = fct_inorder(agec)) %>% 
   print()
 
-
-colors <- hex_to_rgba(inferno(length(brks) - 1, end = 0.9))
+colors <- inferno(length(brks) - 1, begin = 0.1, end = .9) %>% 
+  hex_to_rgba() %>% 
+  rev()
 
 hchart(datag, "area", hcaes(year, pop, group = agec),
        color = colors, showInLegend = FALSE) %>% 
   hc_tooltip(table = TRUE) %>% 
   hc_plotOptions(
     series = list(
+      lineColor = "white",
+      lineWidth = 0.5,
       stacking = "normal",
       marker = list(symbol = "circle", size = 0.1, enabled = FALSE, lineWidth = 0)
       )
@@ -82,15 +86,15 @@ hchart(datag, "area", hcaes(year, pop, group = agec),
 #' now 
 #' 
 #' 
-paltte <- scales::col_numeric(str_sub(inferno(100), 0, -3),
-                              domain = sort(unique(datag$birth_year)))
+paltte <- scales::col_numeric(
+  str_sub(magma(100), 0, -3),
+  # cm.colors(10, alpha = 1),
+  domain = seq(min(datag$birth_year), max(datag$birth_year)))
 
 datag <- mutate(datag, segmentColor = paltte(birth_year))
-datag
 
-
-hc <- hchart(datag, "coloredarea", hcaes(year, pop, group = agec),
-             color = "#444", showInLegend = FALSE) %>%
+hchart(datag, "coloredarea", hcaes(year, pop, group = agec, segmentColor = segmentColor),
+       color = "#444", showInLegend = FALSE) %>%
   hc_tooltip(table = TRUE) %>% 
   hc_plotOptions(
     series = list(
@@ -99,18 +103,6 @@ hc <- hchart(datag, "coloredarea", hcaes(year, pop, group = agec),
     )
   )
 
-library(htmltools)
-dep <-  htmlDependency(
-  name = "multicolor_series",
-  version = "1.1.0",
-  src = c(
-    href = "http://blacklabel.github.io/multicolor_series/js/"
-  ),
-  script = "multicolor_series.js"
-)
 
-hc$dependencies <- c(hc$dependencies, list(dep))
-
-hc
 
 
