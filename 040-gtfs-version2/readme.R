c35d35f1534f790cc40484186e2ecabc2295886c
Joshua Kunst <jbkunst@gmail.com>
2017-03-07 00:35:07 -0300

    now nownow

--- 040-gtfs-version2/readme.R
+++ 040-gtfs-version2/readme.R
@@ -11,182 +11,94 @@
 #### setup ws packages ####
 rm(list = ls())
 knitr::opts_chunk$set(message = FALSE, warning = FALSE)
-library("dplyr")
-library("readr")
-library("stringr")
-library("purrr")
-library("ggplot2")
-library("highcharter")
-
+library(tidyverse)
+library(stringr)
+library(widyr)
+library(igraph)
+library(highcharter)
 
 #'
 #' Read data
 #' 
+#' http://www.dtpm.gob.cl/index.php/2013-04-29-20-33-57/matrices-de-viaje
 
-# http://www.dtpm.gob.cl/index.php/2013-04-29-20-33-57/matrices-de-viaje
-zipfile <- tempfile(fileext = "gtfs.zip")
-download.file("http://datos.gob.cl/dataset/8583ae05-d62f-42d7-a56d-4f865711b65a/resource/dc31a5c7-e717-4b9d-a9a6-93b4d4d2aaca/download/gtfsv17.zip",
-              zipfile, mode = "wb")
 
-fls <- unzip(zipfile, list = TRUE)
 
-for (fl in fls$Name) {
-  
-  dfname <- str_c("df", str_replace(fl, "\\.txt", ""))
-  assign(dfname, read_csv(unz(zipfile, fl)))
-  
+urlfile <- "http://www.dtpm.gob.cl/descargas/archivos/2015.04_Subidas_paradero_mediahora_web.rar"
+rarfile <- file.path("data", basename(urlfile))
+
+if(!file.exists(rarfile)) {
+  dir.create("data")
+  download.file(urlfile, file.path("data", basename(urlfile)), mode = "wb")
 }
 
-rm(fls, fl, dfname, zipfile)
-# fix
-names(dfroutes)[1] <- "route_id"
-names(dfstops)[1] <- "stop_id"
+data <- read_csv2("data/2015.04_Subidas_paradero_mediahora_web/2015.04_Subidas_paradero_mediahora_web.csv")
+ 
+data <- data %>% 
+  mutate(subidas_laboral_promedio = as.numeric(subidas_laboral_promedio)) %>% 
+  filter(!str_detect(paraderosubida, "^(T|L|I|E)?-")) %>% 
+  mutate(paraderosubida = str_to_title(paraderosubida))
 
-routesMetro <- dfroutes %>%
-  filter(agency_id == "M") %>% 
-  mutate(route_id = str_replace(route_id, "_.*", ""),
-         route_color = str_c("#", route_color),
-         route_text_color = str_c("#", route_text_color)) %>% 
-  select(route_id, route_long_name, route_color, route_text_color, route_url)
+count(count(data, paraderosubida), n)
 
-metrocols <- setNames(routesMetro$route_color, routesMetro$route_id)
+data <- mutate(data, mediahora = as.numeric(mediahora))
 
-shapesMetro <- dfshapes %>% 
-  filter(str_detect(shape_id, "^L"), str_detect(shape_id, "-R_")) %>% 
-  mutate(shape_id = str_replace(shape_id, "-R.*", "")) %>%
-  arrange(shape_id, shape_pt_sequence)
+data <- complete(data, paraderosubida, mediahora,
+                 fill = list(subidas_laboral_promedio = 0)) 
 
-stropsMetro <- dfstops %>% 
-  filter(!is.na(stop_url))
+data <- filter(data, mediahora != 0)
 
-ggplot() + 
-  geom_path(data = shapesMetro, aes(shape_pt_lon, shape_pt_lat, group = shape_id, color = shape_id),
-            size = 2) + 
-  geom_point(data = stropsMetro, aes(stop_lon, stop_lat), color = "black", size = 2) + 
-  geom_point(data = stropsMetro, aes(stop_lon, stop_lat), color = "white", size = 1) +
-  scale_color_manual(NULL, values = metrocols) + 
-  coord_equal() + 
-  ggthemes::theme_map() +
-  theme(legend.position = "bottom",  legend.key = element_blank())
+glimpse(data)
 
 
-dsstops <- stropsMetro %>% 
-  select(x = stop_lon, y = stop_lat, url = stop_url, name = stop_name)
+dcor <- data %>%
+  pairwise_cor(paraderosubida, mediahora, subidas_laboral_promedio,
+               upper = FALSE)
 
-dsshapes <- left_join(shapesMetro, routesMetro, by = c("shape_id" = "route_id")) %>% 
-  group_by(shape_id) %>% 
-  do(serie = list(
-    name = first(.$shape_id),
-    color = first(.$route_color),
-    data = list.parse2(data_frame(.$shape_pt_lon, .$shape_pt_lat)),
-    lineWidth = 3,
-    enableMouseTracking = FALSE,
-    marker = list(enabled = FALSE)
-  )) %>% 
-  .$serie
+# hchart(dcor, "heatmap", hcaes(item1, item2, value = correlation))
 
-stoptooltip <- list(
-  pointFormat = "{point.name}<br>{point.stop_url}<a href=\"{point.stop_url}\">url</a>",
-  useHTML = TRUE
-)
+dcor1 <- dcor %>%
+  arrange(desc(correlation)) %>%
+  filter(row_number() <= 400)
 
-stopsmarkeropts <- list(
-  enabled = TRUE,
-  fillColor = "white",
-  lineWidth = 1,
-  radius = 3,
-  lineColor = "#545454",
-  states = list(
-    hover = list(
-      fillColor = "white",
-      lineColor = "#545454"
+g <- graph_from_data_frame(dcor1, directed = FALSE)
+
+E(g)$weight <- dcor1$correlation
+
+dvert <- data_frame(paraderosubida = V(g)$name) %>% 
+  left_join(data %>%
+              group_by(paraderosubida) %>%
+              summarise(n = sum(subidas_laboral_promedio))) %>% 
+  left_join(data %>% 
+              group_by(paraderosubida) %>% 
+              summarise(tend = cor(seq(1, 37), subidas_laboral_promedio)))
+
+wc <- cluster_edge_betweenness (g)
+nc <- length(unique(membership(wc)))
+
+
+V(g)$label <- V(g)$name
+V(g)$size <- dvert$n
+V(g)$color <- colorize(dvert$tend)
+V(g)$comm <- membership(wc)
+
+
+set.seed(1)
+hchart(g) %>% 
+  hc_add_theme(
+    hc_theme_elementary(
+      yAxis = list(visible = FALSE),
+      xAxis = list(visible = FALSE)
     )
   )
-)
 
-highchart() %>%
-  hc_chart(type = "scatter") %>% 
-  hc_add_series_list(dsshapes) %>% 
-  hc_add_series_df(dsstops, name = "Stations",
-                   marker = stopsmarkeropts,
-                   tooltip = stoptooltip) %>% 
-    hc_add_theme(hc_theme_null())
-
-#' 
-#' Nice!
-#' 
-
-
-
-
-
-library("geojsonio")
-
-stgogeojsonurl  <- "http://cedeusdata.geosteiniger.cl/geoserver/wfs?srsName=EPSG%3A4326&typename=geonode%3Acomunas_1&outputFormat=json&version=1.0.0&service=WFS&request=GetFeature"
-
-stgo <- jsonlite::fromJSON(stgogeojsonurl, simplifyVector = FALSE)
-
-stations <- stropsMetro %>% 
-  geojson_json(lat = "stop_lat", lon = "stop_lon")
-
-lines <- left_join(shapesMetro, routesMetro, by = c("shape_id" = "route_id")) %>% 
-  group_by(shape_id) %>% 
-  do(serie = list(
-    name = first(.$shape_id),
-    color = first(.$route_color),
-    data = geojson_json(as.matrix(.[ ,  c("shape_pt_lat","shape_pt_lon")]),
-                        lat = "shape_pt_lat", lon = "shape_pt_lon",
-                        geometry = "polygon"),
-    lineWidth = 4,
-    enableMouseTracking = FALSE,
-    marker = list(enabled = FALSE),
-    type = "mapline"
-  )) %>% 
-  .$serie
-
-highchart(type = "map") %>% 
-  # this is needed to the map dont lost the 1:1 scale
-  hc_add_series(mapData = stgogeojsonurl, showInLegend = FALSE,
-                nullColor = "#A9CF54") %>% 
-  hc_add_series(data = dsstops, type = "mappoint",
-                name = "Stations", marker = stopsmarkeropts,
-                tooltip = list(pointFormat = "{point.properties.stop_name}")) %>% 
-  hc_add_series_list(lines)
-
-stoptooltip <- list(
-  pointFormat = "{point.name}",
-  useHTML = TRUE
-)
-
-stopsmarkeropts <- list(
-  enabled = TRUE,
-  fillColor = "white",
-  lineWidth = 1,
-  radius = 3,
-  lineColor = "#545454",
-  states = list(
-    hover = list(
-      fillColor = "white",
-      lineColor = "#545454"
-    )
-  )
-)
-
-highchart(type = "map") %>% 
-  hc_add_series(data = dsstops, type = "mappoint",
-                name = "Stations", marker = stopsmarkeropts,
-                tooltip = list(pointFormat = "{point.properties.stop_name}"))
-
-hc_add_series(data = marine, type = "mapline",  lineWidth = 2,
-              name = "Marine currents", color = 'rgba(0, 0, 80, 0.33)',
-              states = list(hover = list(color = "#BADA55")),
-              tooltip = list(pointFormat = "{point.properties.NOMBRE}")) 
-
-
-highchart() %>%
-  hc_chart(type = "scatter") %>% 
-  hc_add_series_list(dsshapes) %>% 
-  hc_add_series_df(dsstops, name = "Stations",
-                   marker = stopsmarkeropts,
-                   tooltip = stoptooltip) %>% 
-  hc_add_theme(hc_theme_null())
+# dcor2 <- dcor %>% 
+#   group_by(item1) %>% 
+#   arrange(desc(correlation)) %>% 
+#   filter(row_number() <= 3) %>% 
+#   ungroup()
+# 
+# g2 <- graph_from_data_frame(dcor2, directed = FALSE)
+# E(g2)$weight <- dcor2$correlation^2
+# 
+# hchart(g2)
--- 040-gtfs-version2/readme2.R
+++ 040-gtfs-version2/readme2.R
@@ -22,6 +22,194 @@ library("highcharter")
 #' ![](http://www.transitwiki.org/TransitWiki/images/f/ff/GTFS_data_model_diagram.PNG)
 
 
+# library(devtools)
+# install_github("dgrtwo/widyr")
+# install_github("drsimonj/corrr")
+# library(widyr)
+# library(corrr)
+# 
+# system.time({
+#   dcor1 <- data %>%
+#     pairwise_cor(paraderosubida, mediahora, subidas_laboral_promedio)
+# })
+# 
+# system.time({
+#   dcor2 <- data %>% 
+#     spread(paraderosubida, subidas_laboral_promedio) %>% 
+#     select(-mediahora) %>% 
+#     correlate() %>% 
+#     stretch() %>% 
+#     filter(!is.na(r))
+# })
+# 
+# glimpse(dcor1)
+# glimpse(dcor2)
+# rm(dcor1, dcor2)
+
+
+fls <- unzip(zipfile, list = TRUE)
+
+for (fl in fls$Name) {
+  
+  dfname <- str_c("df", str_replace(fl, "\\.txt", ""))
+  assign(dfname, read_csv(unz(zipfile, fl)))
+  
+}
+
+rm(fls, fl, dfname, zipfile)
+# fix
+names(dfroutes)[1] <- "route_id"
+names(dfstops)[1] <- "stop_id"
+
+routesMetro <- dfroutes %>%
+  filter(agency_id == "M") %>% 
+  mutate(route_id = str_replace(route_id, "_.*", ""),
+         route_color = str_c("#", route_color),
+         route_text_color = str_c("#", route_text_color)) %>% 
+  select(route_id, route_long_name, route_color, route_text_color, route_url)
+
+metrocols <- setNames(routesMetro$route_color, routesMetro$route_id)
+
+shapesMetro <- dfshapes %>% 
+  filter(str_detect(shape_id, "^L"), str_detect(shape_id, "-R_")) %>% 
+  mutate(shape_id = str_replace(shape_id, "-R.*", "")) %>%
+  arrange(shape_id, shape_pt_sequence)
+
+stropsMetro <- dfstops %>% 
+  filter(!is.na(stop_url))
+
+ggplot() + 
+  geom_path(data = shapesMetro, aes(shape_pt_lon, shape_pt_lat, group = shape_id, color = shape_id),
+            size = 2) + 
+  geom_point(data = stropsMetro, aes(stop_lon, stop_lat), color = "black", size = 2) + 
+  geom_point(data = stropsMetro, aes(stop_lon, stop_lat), color = "white", size = 1) +
+  scale_color_manual(NULL, values = metrocols) + 
+  coord_equal() + 
+  ggthemes::theme_map() +
+  theme(legend.position = "bottom",  legend.key = element_blank())
+
+
+dsstops <- stropsMetro %>% 
+  select(x = stop_lon, y = stop_lat, url = stop_url, name = stop_name)
+
+dsshapes <- left_join(shapesMetro, routesMetro, by = c("shape_id" = "route_id")) %>% 
+  group_by(shape_id) %>% 
+  do(serie = list(
+    name = first(.$shape_id),
+    color = first(.$route_color),
+    data = list.parse2(data_frame(.$shape_pt_lon, .$shape_pt_lat)),
+    lineWidth = 3,
+    enableMouseTracking = FALSE,
+    marker = list(enabled = FALSE)
+  )) %>% 
+  .$serie
+
+stoptooltip <- list(
+  pointFormat = "{point.name}<br>{point.stop_url}<a href=\"{point.stop_url}\">url</a>",
+  useHTML = TRUE
+)
+
+stopsmarkeropts <- list(
+  enabled = TRUE,
+  fillColor = "white",
+  lineWidth = 1,
+  radius = 3,
+  lineColor = "#545454",
+  states = list(
+    hover = list(
+      fillColor = "white",
+      lineColor = "#545454"
+    )
+  )
+)
+
+highchart() %>%
+  hc_chart(type = "scatter") %>% 
+  hc_add_series_list(dsshapes) %>% 
+  hc_add_series_df(dsstops, name = "Stations",
+                   marker = stopsmarkeropts,
+                   tooltip = stoptooltip) %>% 
+  hc_add_theme(hc_theme_null())
+
+#' 
+#' Nice!
+#' 
+
+
+
+
+
+library("geojsonio")
+
+stgogeojsonurl  <- "http://cedeusdata.geosteiniger.cl/geoserver/wfs?srsName=EPSG%3A4326&typename=geonode%3Acomunas_1&outputFormat=json&version=1.0.0&service=WFS&request=GetFeature"
+
+stgo <- jsonlite::fromJSON(stgogeojsonurl, simplifyVector = FALSE)
+
+stations <- stropsMetro %>% 
+  geojson_json(lat = "stop_lat", lon = "stop_lon")
+
+lines <- left_join(shapesMetro, routesMetro, by = c("shape_id" = "route_id")) %>% 
+  group_by(shape_id) %>% 
+  do(serie = list(
+    name = first(.$shape_id),
+    color = first(.$route_color),
+    data = geojson_json(as.matrix(.[ ,  c("shape_pt_lat","shape_pt_lon")]),
+                        lat = "shape_pt_lat", lon = "shape_pt_lon",
+                        geometry = "polygon"),
+    lineWidth = 4,
+    enableMouseTracking = FALSE,
+    marker = list(enabled = FALSE),
+    type = "mapline"
+  )) %>% 
+  .$serie
+
+highchart(type = "map") %>% 
+  # this is needed to the map dont lost the 1:1 scale
+  hc_add_series(mapData = stgogeojsonurl, showInLegend = FALSE,
+                nullColor = "#A9CF54") %>% 
+  hc_add_series(data = dsstops, type = "mappoint",
+                name = "Stations", marker = stopsmarkeropts,
+                tooltip = list(pointFormat = "{point.properties.stop_name}")) %>% 
+  hc_add_series_list(lines)
+
+stoptooltip <- list(
+  pointFormat = "{point.name}",
+  useHTML = TRUE
+)
+
+stopsmarkeropts <- list(
+  enabled = TRUE,
+  fillColor = "white",
+  lineWidth = 1,
+  radius = 3,
+  lineColor = "#545454",
+  states = list(
+    hover = list(
+      fillColor = "white",
+      lineColor = "#545454"
+    )
+  )
+)
+
+highchart(type = "map") %>% 
+  hc_add_series(data = dsstops, type = "mappoint",
+                name = "Stations", marker = stopsmarkeropts,
+                tooltip = list(pointFormat = "{point.properties.stop_name}"))
+
+hc_add_series(data = marine, type = "mapline",  lineWidth = 2,
+              name = "Marine currents", color = 'rgba(0, 0, 80, 0.33)',
+              states = list(hover = list(color = "#BADA55")),
+              tooltip = list(pointFormat = "{point.properties.NOMBRE}")) 
+
+
+highchart() %>%
+  hc_chart(type = "scatter") %>% 
+  hc_add_series_list(dsshapes) %>% 
+  hc_add_series_df(dsstops, name = "Stations",
+                   marker = stopsmarkeropts,
+                   tooltip = stoptooltip) %>% 
+  hc_add_theme(hc_theme_null())
+
 #'
 #' Read data
 #' 
