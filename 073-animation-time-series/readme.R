a49d131aeaac0d06f5bc5847e33f4943188456fa
Joshua Kunst <jbkunst@gmail.com>
2018-01-26 12:06:23 -0300

    hope some day i will post something fun!

--- 073-animation-time-series/readme.R
+++ 073-animation-time-series/readme.R
@@ -1,14 +1,12 @@
-library(dplyr)
-library(purrr)
-library(stringr)
+library(tidyverse)
 library(highcharter)
 options(highcharter.theme = hc_theme_smpl())
 
-n <- 100
+n <- 50
 set.seed(123)
 noise <- rnorm(n)
 
-phis <- round(seq(-.90, .90, by = .05), 2)
+phis <- round(seq(-.90, .90, by = .1), 2)
 
 data <- phis %>% 
   map_df(function(ar){
@@ -21,31 +19,34 @@ data <- phis %>%
 data
 
 dmotion <- data %>% 
+  select(index, y) %>% 
   group_by(index) %>% 
-  do(sequence = .$y)
+  nest() %>% 
+  mutate(data = map(data, pull, 1)) %>% 
+  rename(sequence = data)
+
+limit <- data %>% 
+  pull(y) %>% 
+  abs() %>% 
+  max() %>% 
+  ceiling()
 
 highchart() %>% 
   hc_add_series(data = dmotion, zIndex = 10) %>%
   hc_add_series(data = data, "line", hcaes(index, y, group = par),
-                showInLegend = FALSE, color = hex_to_rgba("gray", 0.2),
+                showInLegend = FALSE, color = hex_to_rgba("gray", 0.1),
                 enableMouseTracking = FALSE) %>% 
-  hc_yAxis(min = min(data$y), max = max(data$y)) %>% 
+  hc_yAxis(min = -limit, max = limit) %>% 
   hc_motion(labels = str_pad(phis, 6), updateInterval = 5,
             magnet = list(step = 0.05))
-  
+
 
 dacf <- data %>% 
   group_by(name = par) %>% 
-  do(data = as.vector(acf(.$x)$acf)) %>% 
+  do(data = as.vector(acf(.$y)$acf)) %>% 
   mutate(color = hex_to_rgba("gray", 0.2))
-
-
 
 highchart() %>% 
   hc_plotOptions(series = list(showInLegend = FALSE)) %>% 
   hc_yAxis(min = -1, max = 1) %>% 
   hc_add_series_list(dacf)
-hchart(dacf, "column", hcaes(y = ))
-  
-
-
