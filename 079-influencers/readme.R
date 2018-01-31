2203f62aae8204bf0f3c1ce899e1f4492a151a2b
Joshua Kunst <jbkunst@gmail.com>
2018-01-31 07:23:49 -0300

    aaaa i dont want to do this plz

--- 079-influencers/readme.R
+++ 079-influencers/readme.R
@@ -2,10 +2,43 @@ library(tidyverse)
 library(rtweet)
 library(memoise)
 
-users <- search_users(q = "#rstats", n = 1000, parse = TRUE)
+
+cfs <- memoise::cache_filesystem(".cache")
+
+my_get_friends <- function(user_id = 7220752) {
+  
+  message(user_id)
+  
+  t0 <- Sys.time()
+  
+  while(TRUE) {
+    
+    message("waiting: ", round(difftime(Sys.time(), t0, units = "mins"), 3), " mins")
+    
+    out <- get_friends(user_id, verbose = TRUE)
+    
+    if(is.data.frame(out)) { break } else { Sys.sleep(0.5) } 
+    
+  }
+  
+  out
+  
+}
+  
+
+m_my_get_friends <- memoise(my_get_friends, cache = cfs)
+
+m_my_get_friends()
+
+
+users <- search_users(q = "#rstats", n = 5000, parse = TRUE)
 users <- unique(users)
 
-users_n_friends <- users %>% 
-  pull(user_id) %>% 
-  get_friends(retryonratelimit = TRUE, verbose = TRUE)
 
+to_rmv <- c("733994123678490625", "733994123678490625", "844152803991994368",
+            "735111228322906112", "804915763")
+
+users_n_friends <- users %>% 
+  filter(!user_id %in% to_rmv) %>% 
+  pull(user_id) %>% 
+  map_df(m_my_get_friends)
