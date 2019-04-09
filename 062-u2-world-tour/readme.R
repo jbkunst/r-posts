43bdeaff573ac71dd616f260e0c99cfad0bed08d
Joshua Kunst <jbkunst@gmail.com>
2019-04-09 18:25:45 -0400

    Update readme.R

--- 062-u2-world-tour/readme.R
+++ 062-u2-world-tour/readme.R
@@ -1,77 +1,125 @@
-+# packages ----------------------------------------------------------------
-rm(list = ls())
-library(dplyr)
-library(tidyr)
+library(tidyverse)
 library(rvest)
-library(stringr)
-library(highcharter)
 library(lubridate)
-library(ggmap)
-library(purrr)
-library(viridisLite)
-options(highcharter.debug = TRUE)
-
-
-
-# data --------------------------------------------------------------------
-html <- read_html("https://en.wikipedia.org/wiki/U2_360%C2%B0_Tour")
-
-df <- html_table(html, fill = TRUE)[[2]] 
-df <- tbl_df(df)
-
-names(df) <- str_replace(str_to_lower(names(df)), " ", "_")
-
 Sys.setlocale("LC_TIME","English")
 
-df <- df %>% 
+
+
+# DATA --------------------------------------------------------------------
+html <- read_html("https://en.wikipedia.org/wiki/U2_360%C2%B0_Tour")
+
+data <- html_table(html, fill = TRUE)[[2]] 
+
+data <- data %>% 
+  tbl_df() %>% 
+  rename_all(str_to_lower) %>% 
+  rename_all(str_replace_all, "\\s+", "_")
+
+glimpse(data)
+
+# remove intermediate leg 
+data <- data %>% 
   mutate(leg = ifelse(str_detect(date, "Leg "), date, NA)) %>% 
   fill(leg) %>% 
-  filter(!str_detect(date, "Leg "),
-         date != "TOTAL") %>% 
-  mutate(attendance2 = str_extract(attendance, ".* "),
-         attendance2 = str_replace_all(attendance2, ",| |/", ""),
-         attendance2 = as.numeric(attendance2),
-         revenue2 =  str_replace_all(revenue, ",|\\$", ""),
-         revenue2 = as.numeric(revenue2),
-         revenue2 = coalesce(revenue2, 0),
-         date2 = as.Date(date, format = "%d %B %Y"),
-         ym = date2 - day(date2) + days(1),
-         leg = str_replace(leg, "\\[\\d+\\]\\[\\d+\\]", ""))
+  filter(!str_detect(date, "Leg "), date != "TOTAL")
 
-glimpse(df)
 
-dfym <- df %>% 
+data <- data %>% 
+  mutate(
+    attendance2 = str_extract(attendance, ".* "),
+    attendance2 = str_replace_all(attendance2, ",| |/", ""),
+    attendance2 = as.numeric(attendance2),
+    revenue2 =  str_replace_all(revenue, ",|\\$", ""),
+    revenue2 = as.numeric(revenue2),
+    revenue2 = coalesce(revenue2, 0),
+    date2 = as.Date(date, format = "%d %B %Y"),
+    ym = date2 - day(date2) + days(1),
+    leg = str_replace(leg, "\\[\\d+\\]\\[\\d+\\]", "")
+    )
+
+glimpse(data)
+
+data <- data %>% 
+  select(
+    leg, date = date2, city, country, venue, 
+    opening_act, attendance = attendance2,
+    revenue = revenue2, ym
+    )
+
+dfym <- data %>% 
   group_by(leg, ym, country, city, venue) %>% 
-  summarise(concerts = n(),
-            attendance = first(attendance2),
-            revenue = first(revenue2)) %>% 
-  mutate(location = paste0(country, ", ", city),
-         z = revenue) %>% 
+  summarise(
+    concerts = n(),
+    attendance = sum(attendance),
+    revenue = sum(revenue)
+    ) %>% 
+  mutate(
+    location = paste0(country, ", ", city),
+    z = revenue
+    ) %>% 
   ungroup()
 
-gcodes <- map(dfym$location, geocode)
-gcodes <- map_df(gcodes, as_data_frame)
+
+
+# GEOCODE -----------------------------------------------------------------
+library(ggmap)
+
+geocode("United States, Minneapolis", source = "dsk")
+
+
+library(furrr)
+
+plan(multiprocess)
+
+
+gcodes <- dfym %>% 
+  pull(location) %>% 
+  future_map(geocode, source = "dsk") %>% 
+  map_df(as_tibble)
+
 
 dfym <- bind_cols(dfym, gcodes)
 
 glimpse(dfym)
 
 
-
 # first chart -------------------------------------------------------------
-world <- hcmap(nullColor = "#424242", borderColor = "gray") %>% 
+library(highcharter)
+
+# library(proj4)
+# robinson <- project(cbind(dfym$lon, dfym$lat), 
+#                     proj = "+proj=robin +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
+# 
+# dfym <- dfym %>% 
+#   mutate(
+#     lon = robinson[,1],
+#     lat = robinson[,2]
+#   )
+
+world <- hcmap(
+  # "custom/world-robinson-lowres",
+  nullColor = "#424242",
+  borderColor = "gray"
+  ) %>% 
   hc_chart(backgroundColor = "#161C20") %>% 
   hc_plotOptions(series = list(showInLegend = FALSE))
 
 world
 
 world %>% 
-  hc_add_series(data = dfym, type = "mapbubble", name = "Concerts",
-                maxSize = "2%", color = "white",
-                tooltip = list(valueDecimals = 0,
-                               valuePrefix = "$",
-                               valueSuffix = " USD")
-                )
+  hc_add_series(
+    data = dfym,
+    type = "mapbubble",
+    mapping = hcaes(x = lon, y = lat, z = z, name = location),
+    name = "Concerts",
+    maxSize = "2%",
+    color = "white",
+    tooltip = list(
+      valueDecimals = 0,
+      valuePrefix = "$",
+      valueSuffix = " USD"
+      )
+  )
 
 
 
@@ -82,11 +130,8 @@ dateseq <- seq(min(dfym$ym), max(dfym$ym), by = "month")
 
 sequences <- map2(dfym$ym, dfym$z, function(x, y){ ifelse(x > dateseq, 0, y)})
 
-
-
-cols <- inferno(10, begin = 0.2)
+cols <- viridis::inferno(12, begin = 0.2)
 scales::show_col(cols)  
-
 
 dfym <- mutate(dfym, sequence = sequences, color = colorize(revenue, cols))
 
@@ -99,5 +144,4 @@ world %>%
   hc_motion(enabled = TRUE, series = 1, labels = dateslabels,
             loop = TRUE, autoPlay = TRUE, 
             updateInterval = 1000, magnet = list(step =  1))
-
 
